2acdd57e5c155cbce071220e6bad4f48536e8775
jazs <lsc.jesuszacarias@gmail.com>
2024-07-04 13:48:08 -0600

    After stash pop

--- FileRama1.java
+++ FileRama1.java
@@ -1,7 +1,7 @@
 public class FileRama1{
 	
 	public static void main(String arg[]){
-		System.out.println("Esto es una rama1");
+		System.out.println("Esto es una rama1: After conflictos");
 	}
 	
 }
