fffe71f0e062660ca5bba646d53b129f050c1043
jazs <lsc.jesuszacarias@gmail.com>
2024-07-04 12:24:38 -0600

    After merge write en Hello4

--- Hello4.java
+++ Hello4.java
@@ -1,7 +1,7 @@
 public class Hello{
 	
 	public static void main(String arg[]){
-		System.out.println("Tag de git: Texto despues de la rama");
+		System.out.println("Tag de git: Texto despues de la rama: after merge write rama");
 	}
 	
 }
