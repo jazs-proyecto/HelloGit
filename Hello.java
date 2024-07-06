b026aa4599c8a0b0a2d7607992efcdb58833006c
jazs <lsc.jesuszacarias@gmail.com>
2024-07-05 19:05:27 -0600

    Update with conexion GitHub

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	
 	public static void main(String arg[]){
-		System.out.println("New Hello World: nueva fotografia");
+		System.out.println("New Hello World: nueva fotografia: GitHub");
 	}
 	
 }
